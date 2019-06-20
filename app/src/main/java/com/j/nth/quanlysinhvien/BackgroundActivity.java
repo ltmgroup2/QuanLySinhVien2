db466e032b6f898f9a2b25d3effd7531583e2ab1
Ninh Cong Minh <congminh281297@gmail.com>
2019-06-21 02:13:27 +0700

    Da sua mot chut

---
+++ app/src/main/java/com/j/nth/quanlysinhvien/BackgroundActivity.java
@@ -0,0 +1,30 @@
+package com.j.nth.quanlysinhvien;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BackgroundActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_background);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    Intent intent = new Intent(BackgroundActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    //overridePendingTransition(R.anim.anim_exit_left,R.anim.anim_enter_left);
+                }
+            }
+        });
+        thread.start();
+
+    }
+}
--- app/src/main/java/com/j/nth/quanlysinhvien/MainActivity.java
+++ app/src/main/java/com/j/nth/quanlysinhvien/MainActivity.java
@@ -1,15 +1,72 @@
 package com.j.nth.quanlysinhvien;
 
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.GridView;
+import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private TabLayout tableLayout;
+    private AppBarLayout appBarLayout;
+    private ViewPager viewPager;
+
+    private GridView gridView;
+    private FloatingActionButton btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tableLayout = findViewById(R.id.tab_layout);
+        appBarLayout = findViewById(R.id.appbar_layout);
+        viewPager = findViewById(R.id.view_pager);
+        gridView = findViewById(R.id.grid_view);
+        btnAdd = findViewById(R.id.btn_add_dialog);
 
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.AddFragment(new StudentFragment(),"Danh Sách Sinh Viên");
+        viewPager.setAdapter(adapter);
+        tableLayout.setupWithViewPager(viewPager);
+
+        // registerForContextMenu(gridView);
+
+        // Xứ lý sụ kiện click float button
+//        btnAdd.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // code xử lý
+//            }
+//        });
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.context_menu,menu);
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.detail:
+                // Chi tiết sinh viên
+                break;
+            case R.id.edit:
+                // Sửa sinh viên
+                break;
+            case R.id.delete:
+                // Xóa sinh viên
+                break;
+        }
+        return true;
     }
 }
